0df23bdff2137c23b5cd203ad860d4b0029b29d6
Max Ohsawa <max.ohsawa@gmail.com>
2024-01-10 14:06:19 -0500

    gets projectsData from Project model

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,6 +1,11 @@
 const viewRouter = require('express').Router();
+const { Project, User } = require('../models');
 
-viewRouter.get('/', (req, res) => {
+viewRouter.get('/', async (req, res) => {
+  const projectsData = await Project.findAll();
+
+  console.log(projectsData);
+
   res.status(200).render('home');
 });
 
